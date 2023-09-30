afa388b62295b9d4d2ed41781a64df1e67fde0b1
ADIK ASUS <sfasfasf@mail.co>
2023-09-30 21:40:45 +0600

    created about section, add script for video, add favicon on the page

--- js/main.js
+++ js/main.js
@@ -5,3 +5,35 @@ $(document).ready(function () {
 		loop: true,
 	})
 })
+
+const videoBtn = document.querySelector('#video-btn')
+const videoPreview = document.querySelector('.video__preview')
+const videoWrapper = document.querySelector('.video')
+const video = document.querySelector('#video-file')
+
+// 	videoWrapper.addEventListener('click', function () {
+// 		if (video.paused) {
+// 			video.play()
+// 			videoPreview.classList.add('none')
+// 			videoWrapper.classList.remove('video__overlay')
+// 			videoBtn.classList.add('none')
+// 		} else {
+// 			video.pause()
+// 			videoBtn.classList.remove('none')
+// 			videoWrapper.classList.add('video__overlay')
+// 		}
+// 	})
+//
+
+videoWrapper.addEventListener('click', function () {
+	if (video.paused) {
+		videoPreview.classList.add('none')
+		videoWrapper.classList.remove('video__overlay')
+		videoBtn.classList.add('none')
+		video.play()
+	} else {
+		videoWrapper.classList.add('video__overlay')
+		videoBtn.classList.remove('none')
+		video.pause()
+	}
+})
