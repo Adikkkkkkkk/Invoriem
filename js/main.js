c63b4b933422e45f62b3a297a6925cccb38193cc
ADIK ASUS <sfasfasf@mail.co>
2023-10-07 18:25:16 +0600

    made video section

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 $(document).ready(function () {
 	$('.owl-carousel').owlCarousel({
-		dots: false,
-		margin: 48,
-		loop: true,
+		responsive: {
+			0: {
+				items: 2,
+				margin: 32,
+				loop: true,
+				dots: false,
+			},
+			601: {
+				items: 2,
+				loop: true,
+			},
+			1001: {
+				items: 3,
+				margin: 48,
+				loop: true,
+				dots: false,
+			},
+		},
 	})
 })
 
@@ -10,20 +25,7 @@ const videoBtn = document.querySelector('#video-btn')
 const videoPreview = document.querySelector('.video__preview')
 const videoWrapper = document.querySelector('.video')
 const video = document.querySelector('#video-file')
-
-// 	videoWrapper.addEventListener('click', function () {
-// 		if (video.paused) {
-// 			video.play()
-// 			videoPreview.classList.add('none')
-// 			videoWrapper.classList.remove('video__overlay')
-// 			videoBtn.classList.add('none')
-// 		} else {
-// 			video.pause()
-// 			videoBtn.classList.remove('none')
-// 			videoWrapper.classList.add('video__overlay')
-// 		}
-// 	})
-//
+const videoMobile = document.querySelector('#video-file-mobile')
 
 videoWrapper.addEventListener('click', function () {
 	if (video.paused) {
@@ -35,5 +37,16 @@ videoWrapper.addEventListener('click', function () {
 		videoWrapper.classList.add('video__overlay')
 		videoBtn.classList.remove('none')
 		video.pause()
+	}
+	// Mobile
+	if (videoMobile.paused) {
+		videoPreview.classList.add('none')
+		videoWrapper.classList.remove('video__overlay')
+		videoBtn.classList.add('none')
+		videoMobile.play()
+	} else {
+		videoWrapper.classList.add('video__overlay')
+		videoBtn.classList.remove('none')
+		videoMobile.pause()
 	}
 })
